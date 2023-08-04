70d34660bfe23a0cadd24bb6e61809642e4dd4fd
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-08-04 14:48:57 +0300

    My twenty first

--- src/app/refs.js
+++ src/app/refs.js
@@ -1,7 +1,7 @@
 export const refs = {
   form: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
-  // btnLoadMore: document.querySelector('.load-more'),
+  btnLoadMore: document.querySelector('.load-more'),
   backdrop: document.querySelector('.backdrop'),
   // spinner: document.querySelector('.js-spinner'),
   body: document.querySelector('body'),
